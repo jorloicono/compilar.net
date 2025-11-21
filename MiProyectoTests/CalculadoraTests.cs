06f5454cdeb823614431308f4f07a739a1b4c066
Jorge López Blasco <115549197+jorloicono@users.noreply.github.com>
2025-11-21 13:25:09 +0100

    Update CalculadoraTests.cs

--- MiProyectoTests/CalculadoraTests.cs
+++ MiProyectoTests/CalculadoraTests.cs
@@ -9,39 +9,21 @@ namespace MiProyectoTests
         private Calculadora calc;
 
         [SetUp]
-        public void Setup()
-        {
-            calc = new Calculadora();
-        }
+        public void Setup() => calc = new Calculadora();
 
         [Test]
-        public void Suma_Correcta()
-        {
-            Assert.AreEqual(8, calc.Sumar(5, 3));
-        }
+        public void Suma_Correcta() => Assert.That(calc.Sumar(5, 3), Is.EqualTo(8));
 
         [Test]
-        public void Resta_Correcta()
-        {
-            Assert.AreEqual(2, calc.Restar(5, 3));
-        }
+        public void Resta_Correcta() => Assert.That(calc.Restar(5, 3), Is.EqualTo(2));
 
         [Test]
-        public void Multiplicacion_Correcta()
-        {
-            Assert.AreEqual(15, calc.Multiplicar(5, 3));
-        }
+        public void Multiplicacion_Correcta() => Assert.That(calc.Multiplicar(5, 3), Is.EqualTo(15));
 
         [Test]
-        public void Division_Correcta()
-        {
-            Assert.AreEqual(1.6666666666666667, calc.Dividir(5, 3), 0.0001);
-        }
+        public void Division_Correcta() => Assert.That(calc.Dividir(5, 3), Is.EqualTo(5.0 / 3.0).Within(0.0001));
 
         [Test]
-        public void DivisionPorCero_LanzaExcepcion()
-        {
-            Assert.Throws<DivideByZeroException>(() => calc.Dividir(5, 0));
-        }
+        public void DivisionPorCero_LanzaExcepcion() => Assert.Throws<DivideByZeroException>(() => calc.Dividir(5, 0));
     }
 }
