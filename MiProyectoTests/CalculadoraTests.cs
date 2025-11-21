371cbfcf6ee7323ece48776318c48bc2561e4639
Jorge López Blasco <115549197+jorloicono@users.noreply.github.com>
2025-11-21 13:24:13 +0100

    Update CalculadoraTests.cs

--- MiProyectoTests/CalculadoraTests.cs
+++ MiProyectoTests/CalculadoraTests.cs
@@ -1,40 +1,47 @@
-﻿using NUnit.Framework;
+using NUnit.Framework;
 using MiProyectoLib;
 using System;
+
 namespace MiProyectoTests
 {
- public class CalculadoraTests
- {
- private Calculadora calc;
- [SetUp]
- public void Setup()
- {
- calc = new Calculadora();
- }
- [Test]
- public void Suma_Correcta()
- {
- Assert.AreEqual(8, calc.Sumar(5,3));
- }
- [Test]
- public void Resta_Correcta()
- {
- Assert.AreEqual(2, calc.Restar(5,3));
- }
- [Test]
- public void Multiplicacion_Correcta()
- {
- Assert.AreEqual(15, calc.Multiplicar(5,3));
- }
- [Test]
- public void Division_Correcta()
- {
- Assert.AreEqual(1.6666666666666667, calc.Dividir(5,3), 0.0001);
- }
- [Test]
- public void DivisionPorCero_LanzaExcepcion()
- {
- Assert.Throws<DivideByZeroException>(() => calc.Dividir(5,0));
- }
- }
+    public class CalculadoraTests
+    {
+        private Calculadora calc;
+
+        [SetUp]
+        public void Setup()
+        {
+            calc = new Calculadora();
+        }
+
+        [Test]
+        public void Suma_Correcta()
+        {
+            Assert.AreEqual(8, calc.Sumar(5, 3));
+        }
+
+        [Test]
+        public void Resta_Correcta()
+        {
+            Assert.AreEqual(2, calc.Restar(5, 3));
+        }
+
+        [Test]
+        public void Multiplicacion_Correcta()
+        {
+            Assert.AreEqual(15, calc.Multiplicar(5, 3));
+        }
+
+        [Test]
+        public void Division_Correcta()
+        {
+            Assert.AreEqual(1.6666666666666667, calc.Dividir(5, 3), 0.0001);
+        }
+
+        [Test]
+        public void DivisionPorCero_LanzaExcepcion()
+        {
+            Assert.Throws<DivideByZeroException>(() => calc.Dividir(5, 0));
+        }
+    }
 }
