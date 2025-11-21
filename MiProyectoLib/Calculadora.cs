be640d48ae937f231d1573d960c1af00613894c6
Jorge López Blasco <115549197+jorloicono@users.noreply.github.com>
2025-11-21 13:29:58 +0100

    Refactor Calculadora class with proper formatting

--- MiProyectoLib/Calculadora.cs
+++ MiProyectoLib/Calculadora.cs
@@ -1,14 +1,17 @@
-﻿namespace MiProyectoLib
+using System;
+
+namespace MiProyectoLib
 {
- public class Calculadora
- {
- public int Sumar(int a, int b) => a + b;
- public int Restar(int a, int b) => a - b;
- public int Multiplicar(int a, int b) => a * b;
- public double Dividir(int a, int b)
- {
- if (b == 0) throw new DivideByZeroException("No se puede dividir entre cero");
- return (double)a / b;
- }
- }
+    public class Calculadora
+    {
+        public int Sumar(int a, int b) => a + b;
+        public int Restar(int a, int b) => a - b;
+        public int Multiplicar(int a, int b) => a * b;
+
+        public double Dividir(int a, int b)
+        {
+            if (b == 0) throw new DivideByZeroException("No se puede dividir entre cero");
+            return (double)a / b;
+        }
+    }
 }
