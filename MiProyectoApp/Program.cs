983989e8c415f11d01a23add0197c453c98d884c
Jorge López Blasco <115549197+jorloicono@users.noreply.github.com>
2025-11-21 12:47:05 +0100

    Add files via upload

---
+++ MiProyectoApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MiProyectoLib;
+namespace MiProyectoApp
+{
+ class Program
+ {
+ static void Main(string[] args)
+ {
+ var calc = new Calculadora();
+ Console.WriteLine($"Suma: {calc.Sumar(5,3)}");
+ Console.WriteLine($"Resta: {calc.Restar(5,3)}");
+ Console.WriteLine($"Multiplicación: {calc.Multiplicar(5,3)}");
+ Console.WriteLine($"División: {calc.Dividir(5,3)}");
+ }
+ }
+}
---
+++ MiProyectoLib/Calculadora.cs
@@ -0,0 +1,14 @@
+﻿namespace MiProyectoLib
+{
+ public class Calculadora
+ {
+ public int Sumar(int a, int b) => a + b;
+ public int Restar(int a, int b) => a - b;
+ public int Multiplicar(int a, int b) => a * b;
+ public double Dividir(int a, int b)
+ {
+ if (b == 0) throw new DivideByZeroException("No se puede dividir entre cero");
+ return (double)a / b;
+ }
+ }
+}
